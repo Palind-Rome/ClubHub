d43eb452c2732bc0a9b1d604412d64a5cd652bdf
YoumiaHina <181430519+YoumiaHina@users.noreply.github.com>
2026-07-07 14:39:27 +0000

    chore(api): 从 openapi.yaml 自动生成 API 代码

---
+++ frontend/src/api/models/index.ts
@@ -0,0 +1,19 @@
+// @ts-nocheck
+/* tslint:disable */
+/* eslint-disable */
+export * from "./Activity";
+export * from "./ApiError";
+export * from "./AssignRoleRequest";
+export * from "./AuthResponse";
+export * from "./AuthRole";
+export * from "./AuthUser";
+export * from "./Club";
+export * from "./CreateClubRequest";
+export * from "./HealthStatus";
+export * from "./LoginRequest";
+export * from "./PermissionCheckResult";
+export * from "./PermissionDefinition";
+export * from "./RegisterRequest";
+export * from "./RoleAssignmentResult";
+export * from "./RoleDefinition";
+export * from "./UpdateClubRequest";
